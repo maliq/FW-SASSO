eabfbb2ac73f27493c90413be362cc80fffad356
maliq <mmaliq@gmail.com>
2016-05-07 19:35:25 -0300

    generate symbolic links for all datasets script

--- experiments/set_links.sh
+++ experiments/set_links.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-declare -a DS_LIST=(Adult) #IJCNN MNIST TIMIT Web)
+declare -a DS_LIST=(Adult IJCNN MNIST TIMIT Web)
 for DS in "${DS_LIST[@]}"
 do
     ln -s ../sasso/experiments/sasso_${DS}.sh sasso_${DS}.sh
